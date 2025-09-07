f6c594e50f13e9a3b19d8948eef1c938f7e63274
Malakai <poncemalakai@gmail.com>
2025-09-07 02:27:30 -0700

    Commit 2 with Resturant struct

--- Lab_3.cpp
+++ Lab_3.cpp
@@ -4,3 +4,11 @@
 #include <vector>
 using namespace std;
 
+struct Restaurant {
+    string name;
+    string address;
+    string cuisine;
+    double rating; //double for decimal ratings
+    int price;     //price level from 1 to 5
+};
+
